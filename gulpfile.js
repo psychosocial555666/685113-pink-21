b6818bebb7a51e1b1729228739966ef734e41869
Александр <psychosocial555666@yandex.ru>
2021-01-12 20:14:45 +0300

    Защита проекта: Попытка 1

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ const clean = () => {
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,12 @@
-let pageHeader = document.querySelector('.page-header');
-let toggleButton = document.querySelector('.main-nav__toggle');
-let mainNav = document.querySelector('.main-nav');
+let pageHeader = document.querySelector(".page-header");
+let toggleButton = document.querySelector(".main-nav__toggle");
+let mainNav = document.querySelector(".main-nav");
 
-pageHeader.classList.add('page-header__menu-closed');
-pageHeader.classList.add('page-header__menu-js');
-mainNav.classList.add('main-nav__closed');
+pageHeader.classList.add("page-header__menu-closed");
+pageHeader.classList.add("page-header__menu-js");
+mainNav.classList.add("main-nav__closed");
 
-
-toggleButton.addEventListener('click', () => {
-  pageHeader.classList.toggle('page-header__menu-closed');
-  mainNav.classList.toggle('main-nav__closed');
+toggleButton.addEventListener("click", () => {
+  pageHeader.classList.toggle("page-header__menu-closed");
+  mainNav.classList.toggle("main-nav__closed");
 })
